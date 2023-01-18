f45ca0050b75b67d0153f97d0d8a925349cb294f
amitmhr <amit.mehra244@gmail.com>
2023-01-19 01:28:49 +0530

    added JS questions answers homework

---
+++ JS/01/variable.js
@@ -0,0 +1,10 @@
+let user = "Amit"; // string type variable
+let admin = true; // boolean type variable
+let token = null; // null type variable
+let data; // undefined variable
+
+//displaying value and datatype of the variables
+console.log(user, typeof(user));
+console.log(admin, typeof(admin));
+console.log(token, typeof(token));
+console.log(data, typeof(data));
---
+++ JS/02/variable.js
@@ -0,0 +1,7 @@
+let firstName = "Amit"
+let lastName = "Mehra"
+let maritalStatus = "un merried"
+let country = "India"
+let age = 28
+
+console.log(`My name is ${firstName} ${lastName} and I am ${maritalStatus}. I belong to ${country} and my age is ${age}`);
